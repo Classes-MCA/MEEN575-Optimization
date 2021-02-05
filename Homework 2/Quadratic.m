b3e7476d7c606f4874552ab6fa7d08ff65cfffc7
Mark C. Anderson <anderson.mark.az@gmail.com>
2021-02-05 06:59:40 -0700

    Finished initial optimizer

--- Homework 2/Quadratic.m
+++ Homework 2/Quadratic.m
@@ -5,16 +5,31 @@ plotStyle('StandardStyle','SonicBoom')
 % Creating an intial guess
 x0 = [5,10];
 
-[xopt, fopt] = uncon(@func,x0,1e-6,'Plot2DFunction',true);
+[xopt, fopt] = uncon(@quad,x0,1e-6,'Plot2DFunction',true);
+
+pause(3)
+
+[xopt, fopt] = uncon(@rosenbrock,x0,1e-6,'Plot2DFunction',true);
 
 
 % Defining the quadratic function
-function  [f,df] = func(x)
+function  [f,df] = quad(x)
 
     beta = 3/2;
 
     f = x(1)^2 + x(2)^2 - beta*x(1)*x(2);
     
     df = [2*x(1) - beta*x(2), 2*x(2) - beta*x(1)];
+    df = df(:);
+
+end
+
+% Defining the quadratic function
+function  [f,df] = rosenbrock(x)
+
+    f = (1 - x(1))^2 + 100*(x(2) - x(1)^2)^2;
+    
+    df = [2*(1 - x(1)) + 200*(x(2) - x(1))*2*x(1), 200*(x(2) - x(1)^2)];
+    df = df(:);
 
 end
--- Homework 2/uncon.m
+++ Homework 2/uncon.m
@@ -38,22 +38,97 @@ p.parse(func, x0, tau, varargin{:});
 
 Plot2DFunction = p.Results.Plot2DFunction;
 
+%----- CONTINUING THE FUNCTION -----%   
+
+% Plotting the 2-dimensional function, if desired
 if Plot2DFunction
     % Create a plot of the space
-    x = -10:0.1:10;
-    y = -10:0.1:10;
-    plotSpace(x,y,func);
+    xpos = -10:0.1:10;
+    ypos = -10:0.1:10;
+    plotSpace(xpos,ypos,func);
 end
 
 % Forcing x0 to be a column vector
 x0 = x0(:);
 
+% Basic line search
+x = x0;
+
+% Perform the optimization
+[xopt, fopt] = optimize(x,func,tau,Plot2DFunction);
 
 
+end
 
-% Setting dummy results
-xopt = 0;
-fopt = 0;
+function [xopt, fopt] = optimize(x,func,tau,Plot2DFunction)
+
+    alpha = 1;
+    xhist = x;
+    [f(1), df(1,:)] = func(x);
+
+    for i = 2:1000
+    
+        [f(i), df(i,:)] = func(x);
+    
+        p = 0.9;
+        direction = -(1-p).*df(i-1,:) - p.*df(i,:);
+        xnew = linesearch(x, alpha, direction, func);
+
+        disp(['Iteration: ',num2str(i-1),', x = [',num2str(xnew(1)),', ',num2str(xnew(2)),']'])
+
+        deltaX = xnew - x;
+        x = xnew;
+        xhist = [xhist, x];
+        
+        if Plot2DFunction
+            
+            subplot(1,2,1)
+            scatter3(xhist(1,:), xhist(2,:), f(:),...
+                    'MarkerEdgeColor','flat',...
+                    'MarkerFaceColor','red',...
+                    'CData',[1,0,0],...
+                    'SizeData',10)
+            line(xhist(1,:), xhist(2,:), f(:))
+                 
+            subplot(1,2,2)
+            scatter(xhist(1,:), xhist(2,:),...
+                    'MarkerEdgeColor','flat',...
+                    'MarkerFaceColor','red',...
+                    'CData',[1,0,0],...
+                    'SizeData',10)
+            line(xhist(1,:), xhist(2,:), f(:))
+            drawnow()
+            
+        end
+
+        if rms(deltaX) < tau
+            disp(['Finished after ',num2str(i-1),' iterations.'])
+            break
+        end
+
+    end
+    
+    xopt = x;
+    fopt = func(x);
+
+end
+
+function xnew = linesearch(x, alpha, direction, func)
+    
+    direction = direction(:); % Force to be a column vector
+
+    for i = 1:100
+        xnew = x + alpha .* direction;
+        
+        if func(xnew) < func(x)
+            return
+        elseif alpha < 0.01
+            return
+        else
+            alpha = alpha / 2; % Shrink the step size
+        end
+        
+    end
 
 end
 
@@ -83,10 +158,12 @@ function [h,ax1,ax2] = plotSpace(x,y,func)
     surf(X,Y,values,...
          'mesh','none')
     title('Surface View')
+    hold on
     
     ax2 = subplot(1,2,2);
     contour(X,Y,values,...
             'LevelStep',10.0)
     title('Contour View')
+    hold on
 
 end
