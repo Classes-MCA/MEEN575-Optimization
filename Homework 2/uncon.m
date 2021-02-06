c946c3f36c67e13d6b5d3a6f6329220e4d71f9b2
Mark C. Anderson <anderson.mark.az@gmail.com>
2021-02-06 13:01:49 -0700

    It all works and makes gif files

--- Homework 2/uncon.m
+++ Homework 2/uncon.m
@@ -38,6 +38,7 @@ p.addParameter('YLims',[-5,5])
 p.addParameter('ContourStep',10)
 p.addParameter('PlotPoints',false)
 p.addParameter('MakeGIF',false)
+p.addParameter('FrameRate',24)
 
 p.parse(func, x0, tau, varargin{:});
 
@@ -47,6 +48,7 @@ YLims = p.Results.YLims;
 ContourStep = p.Results.ContourStep;
 PlotPoints = p.Results.PlotPoints;
 MakeGIF = p.Results.MakeGIF;
+FrameRate = p.Results.FrameRate;
 
 %----- CONTINUING THE FUNCTION -----%   
 
@@ -67,33 +69,33 @@ x0 = x0(:);
 x = transpose(x0); % Making a row vector
 
 % Perform the optimization
-[xopt, fopt] = optimize(x,func,tau,Plot2DFunction,PlotPoints,MakeGIF);
+[xopt, fopt] = optimize(x,func,tau,Plot2DFunction,PlotPoints,MakeGIF,FrameRate);
 
 
 end
 
-function [xopt, fopt] = optimize(x,func,tau,Plot2DFunction,PlotPoints,MakeGIF)
+function [xopt, fopt] = optimize(x,func,tau,Plot2DFunction,PlotPoints,MakeGIF,FrameRate)
 
-    [f(1),pold,~] = getFunctionInfo(x,func); % Starting function value
+    [~,pold,~] = getFunctionInfo(x,func); % Starting function value
 
     
-    for i = 2:2e4
+    for i = 1:2e4
         
-        visualize(Plot2DFunction,PlotPoints,x,f,i,MakeGIF);
-        
-        [f(i),p,tol] = getFunctionInfo(x(i-1,:),func);
-        
-        x(i,:) = getNewPoint(x(i-1,:),p,f(i),func,pold);
-        
-        pold = p;
+        [f(i),p,tol] = getFunctionInfo(x(i,:),func);
         
         printinfo(i,tol,f(i),x(i,:))
+        
+        visualize(Plot2DFunction,PlotPoints,x,f,i,MakeGIF,FrameRate);
         
         if tol < tau
             xopt = x(i,:);
             fopt = f(i);
             return
         end
+        
+        x(i+1,:) = getNewPoint(x(i,:),p,f(i),func,pold);
+        
+        pold = p;
         
     end
     
@@ -116,7 +118,7 @@ function xnew = getNewPoint(x,pnew,fold,func,pold)
     
     x = x(:); % Make column vector
 
-    alpha = 10;
+    alpha = 1;
     
     gradientAmounts = 0.5:0.1:1.0; % Different amounts of the direction to try
     
@@ -128,7 +130,7 @@ function xnew = getNewPoint(x,pnew,fold,func,pold)
         % if w = 1, then the old direction is ignored
         p_weighted = (1-w)*pold + w*pnew;
     
-        for j = 1:20
+        for j = 1:50
             xnew = x + alpha*p_weighted; % Get new x value
 
             fnew = func(xnew); % See what the function value is there
@@ -162,15 +164,26 @@ function printinfo(iteration,tolerance,f,x)
 
 end
 
-function plotCurrentStatus(x,f)
+function plotCurrentStatus(x,f,i)
 
     subplot(1,2,1)
+    if i > 1
+        ax = gca;
+        delete(ax.Children(1:2)); % Remove top scatter and line plots
+    end
     scatter3(x(:,1), x(:,2), f,...
-             'r.')
+             'SizeData',100,...
+             'MarkerFaceColor',[1 0 0])
+    line(x(:,1), x(:,2), f)
          
     subplot(1,2,2)
-    scatter(x(:,1), x(:,2),...
-            'r.')
+    if i > 1
+        ax = gca;
+        delete(ax.Children(1:2)); % Remove top scatter and line plots
+    end
+    scatter(x(:,1), x(:,2),100,...
+            'MarkerFaceColor',[1 0 0])
+    line(x(:,1), x(:,2))
 
 end
 
@@ -189,27 +202,27 @@ function createGraph(x)
 
 end
 
-function visualize(Plot2DFunction,PlotPoints,x,f,i,MakeGIF)
+function visualize(Plot2DFunction,PlotPoints,x,f,i,MakeGIF,FrameRate)
 
     if Plot2DFunction
             
-        plotCurrentStatus(x,f)
+        plotCurrentStatus(x,f,i)
         drawnow()
 
     elseif PlotPoints
 
-        createGraph(x(i-1,:))
+        createGraph(x(i,:))
         drawnow()
         
     end
     
     if MakeGIF
-        createGIF(i)
+        createGIF(i,FrameRate)
     end
     
 end
         
-function createGIF(i)
+function createGIF(i,FrameRate)
 
     frame = getframe(gcf);
     im = frame2im(frame);
@@ -218,10 +231,10 @@ function createGIF(i)
     filename = "OptimizationGIF.gif";
     
     % Write to the GIF File 
-    if i == 2 
-        imwrite(imind,cm,filename,'gif', 'DelayTime',1/96, 'Loopcount',inf); 
+    if i == 1
+        imwrite(imind,cm,filename,'gif', 'DelayTime',1/FrameRate, 'Loopcount',inf); 
     else 
-        imwrite(imind,cm,filename,'gif', 'DelayTime',1/96,'WriteMode','append'); 
+        imwrite(imind,cm,filename,'gif', 'DelayTime',1/FrameRate,'WriteMode','append'); 
     end 
 
 end
@@ -255,13 +268,15 @@ function [h,ax1,ax2] = plotSpace(x1,x2,func,ContourStep)
     [X1,X2] = meshgrid(x1,x2);
     surf(X1,X2,values,...
          'mesh','none')
-    title('Surface View')
+    title('Surface View',...
+          'FontSize',14)
     hold on
     
     ax2 = subplot(1,2,2);
     contour(X1,X2,values,...
             'LevelStep',ContourStep)
-    title('Contour View')
+    title('Contour View',...
+          'FontSize',14)
     hold on
 
 end
